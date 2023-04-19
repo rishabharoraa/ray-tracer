17b14cd2a2d3530d9189a420a41349a0216d9641
rishabharoraa <rishabharora.ns@gmail.com>
2023-04-20 00:11:52 +0530

    added first image render

--- main.go
+++ main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+const ImageWidth = 256;
+const ImageHeight = 256;
+
+type point struct {
+    x float32;
+    y float32;
+    z float32;
+}
+
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println("P3\n", ImageWidth, " ", ImageHeight, "\n255\n")
+    for i := 0; i < ImageHeight; i++ {
+        for j := 0; j < ImageWidth; j++ {
+            r := float32(i) / (ImageHeight-1);
+            g := float32(j) / (ImageWidth-1);
+            b := 0.25;
+
+            ir := int(255.999 * r);
+            ig := int(255.999 * g);
+            ib := int(255.999 * b);
+            fmt.Print(ir, " ", ig, " ", ib, " ", "\n");
+        }
+    }
 }
